5a0d39badad2546b0de8a17e085d24d8ffbbeee8
AlexLee1999 <chunyen.alex.lee@gmail.com>
2021-07-11 02:01:34 +0800

    add solution

---
+++ algorithm/C++/insert_interval.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> res;
+        if(intervals.empty()){
+            res.push_back(newInterval);
+            return res;
+        }
+        int count = 0;
+        while(count < intervals.size() && intervals[count][1] < newInterval[0]){
+                res.push_back(intervals[count]);
+                count++;
+        }
+        while(count < intervals.size() && intervals[count][1] >= newInterval[0] && intervals[count][0] <= newInterval[1]){
+                newInterval[0] = min(intervals[count][0], newInterval[0]);
+                newInterval[1] = max(intervals[count][1], newInterval[1]);
+                count++;
+        }
+        res.push_back(newInterval);
+        while(count < intervals.size() && intervals[count][0] > newInterval[1]){
+                res.push_back(intervals[count]);
+                count++;
+        }
+
+        return res;
+    }
+};
---
+++ algorithm/C++/search_in_rotated_sorted_array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid;
+        while (left <= right){
+            mid = (left + right) / 2;
+            cout<<left<<'-'<<mid<<'-'<<right<<' ';
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[left] <= nums[mid]){ //left-mid is sorted
+                if(nums[mid] > target && target >= nums[left]){
+                    right = mid - 1;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+            else{ //mid- right is sorted
+                if(nums[mid] < target && target <= nums[right]){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
