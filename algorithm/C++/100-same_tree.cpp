8313fe772e4c202830fdc3f8c7b8429316c030a9
AlexLee1999 <chunyen.alex.lee@gmail.com>
2021-07-12 00:38:34 +0800

    add c++

---
+++ algorithm/C++/100-same_tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool is_true(TreeNode* a, TreeNode* b){
+        if(a == nullptr && b==nullptr){
+            return true;
+        }
+        if((a == nullptr && b!=nullptr) ||(a != nullptr && b==nullptr) || (a->val != b->val)){
+            return false;
+        }
+        return is_true(a->left, b->left)&&is_true(a->right, b->right);
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return is_true(p, q);
+    }
+};
---
+++ algorithm/C++/102-binary_tree_level_order_traversal.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if(root == nullptr){
+            return res;
+        }
+        queue<TreeNode*> que;
+        que.push(root);
+        TreeNode* tmp;
+        while(!que.empty()){
+            vector<int> res_tmp;
+            int n = que.size();
+            for (int i=0; i<n; ++i){
+                tmp = que.front();
+                res_tmp.push_back(tmp->val);
+
+
+                if(tmp->left != nullptr){
+                    que.push(tmp->left);
+                }
+                if(tmp->right != nullptr){
+                    que.push(tmp->right);
+                }
+                que.pop();
+            }
+            res.push_back(res_tmp);
+        }
+        return res;
+    }
+};
