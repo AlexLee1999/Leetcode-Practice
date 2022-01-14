cb20b034fe91da7d41539a924692dfee68c56f6e
Alex Lee <chunyen.alex.lee@gmail.com>
2022-01-15 00:37:33 +0800

    add solutions
    8

---
+++ algorithm/C++/0008-String_to_Integer_atoi.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int ptr = 0;
+        while (ptr < s.size() && s[ptr] == ' ') {
+            ptr ++;
+        }
+        int sign = 1;
+        if (s[ptr] == '-') {
+            sign = -1;
+            ptr ++;
+        }
+        else if (s[ptr] == '+') {
+            sign = 1;
+            ptr ++;
+        }
+        int result = 0;
+        while (ptr < s.size() && s[ptr] - '0' >= 0 && s[ptr] - '0' <= 9) {
+            int digit = (s[ptr] - '0') * sign;
+            if (sign == 1) {
+                if ((INT_MAX / 10 < result) || (INT_MAX / 10 == result && INT_MAX % 10 < digit)) {
+                    return INT_MAX;
+                }
+            }
+            else if (sign == -1) {
+                if ((INT_MIN / 10 > result) || (INT_MIN / 10 == result && INT_MIN % 10 > digit)) {
+                    return INT_MIN;
+                }
+            }
+            result *= 10;
+            result += digit;
+            ptr ++;
+        }
+        return result;
+    }
+};
